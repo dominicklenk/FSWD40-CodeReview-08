e5b1258e752e1e9a99d321fdf5a694c74fc14e8d
dominicklenk <dominic_klenk@yahoo.at>
2018-06-02 17:31:13 +0200

    revised

--- dating-app/.history/src/app/Services/firebase.services_20170811122422.ts
+++
@@ -1,3 +0,0 @@
-import {Injectable} from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import 'rxjs/add/operator/map';
--- dating-app/.history/src/app/Services/firebase.services_20170811122453.ts
+++
@@ -1,8 +0,0 @@
-import {Injectable} from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import 'rxjs/add/operator/map';
-
-@Injectable()
-export class FirebaseService{
-    
-}
